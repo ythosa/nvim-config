20c54efd27e8937f3284b8082bd5c407f817f61c
Babin Ruslan <vasus714@yandex.ru>
2024-11-12 00:34:39 +0300

    feat: added mappings to search in nvim-tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,3 +4,22 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
+map("n", "<leader>fft", function() 
+  local api = require('nvim-tree.api')
+  local node = api.tree.get_node_under_cursor()
+  local path = node.absolute_path
+  require('telescope.builtin').find_files {
+      prompt_title = "Search in " .. path,
+      cwd = path,
+  }
+end, { desc = "Search files in selected tree node" })
+map("n", "<leader>fwt", function() 
+  local api = require('nvim-tree.api')
+  local node = api.tree.get_node_under_cursor()
+  local path = node.absolute_path
+  require('telescope.builtin').live_grep {
+      prompt_title = "Search in " .. path,
+      cwd = path,
+  }
+end, { desc = "Search strings in selected tree node" })
+
