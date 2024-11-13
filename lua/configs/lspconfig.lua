d57803f65a27a4ab165159c840d4c6f32050e2fa
Babin Ruslan <vasus714@yandex.ru>
2024-11-13 03:04:16 +0300

    feat: added ls for proto

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -11,7 +11,7 @@ local ooo = function(client, bufnr)
 end
 
 -- lsps with default config
-local servers = { "html", "cssls", "clangd" }
+local servers = { "html", "cssls", "buf_ls" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = ooo,
