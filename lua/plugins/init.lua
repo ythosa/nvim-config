9cecbd32f39faaddb58f1cc76e3a6c859b0e2b47
Babin Ruslan <vasus714@yandex.ru>
2025-02-02 02:57:31 +0300

    feat: turned on auto brackets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -249,8 +249,6 @@ return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true,
-    enabled = false,
-    -- use opts = {} for passing setup options, this is equivalent to setup({}) function
   },
   {
     "RRethy/vim-illuminate",
