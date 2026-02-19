b56bd5e1890a4b4e3db37661a360bbedebf70de1
rbabin <rbabin@ozon.ru>
2026-02-19 18:48:51 +0300

    fix: removed remains from base46 & nvchad

--- lua/plugins/lsp/configs/lspconfig.lua
+++ lua/plugins/lsp/configs/lspconfig.lua
@@ -89,7 +89,6 @@ local defaults = function()
                     library = {
                         vim.fn.expand "$VIMRUNTIME/lua",
                         vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-                        vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
                         vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
                         "${3rd}/luv/library",
                     },
